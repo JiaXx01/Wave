d66dd8781768f963fb810ef776066d09d683f201
JiaX <18350569369@163.coom>
2024-04-29 11:10:19 +0800

    前端：修改方法名findNotes

--- apps/web/src/lib/api/note.ts
+++ apps/web/src/lib/api/note.ts
@@ -6,7 +6,7 @@ export const createNote = async (): Promise<NoteInfo> => {
   return http.post('/note').then(res => res.data)
 }
 
-export const getNotes = async (): Promise<NoteInfo[]> => {
+export const findNotes = async (): Promise<NoteInfo[]> => {
   return http.get('/note').then(res => res.data)
 }
 
