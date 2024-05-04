e257a901115d8e2f0819956e16a6834bef0cfa1d
JiaX <18350569369@163.coom>
2024-05-04 16:16:25 +0800

    后端：数据库：修改file表

--- apps/web/src/router/page/file/Files.tsx
+++ apps/web/src/router/page/file/Files.tsx
@@ -1,37 +1,47 @@
 import { Fragment } from 'react'
-import { useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import {
   Breadcrumb,
   BreadcrumbItem,
-  // BreadcrumbLink,
+  BreadcrumbLink,
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator
-  // BreadcrumbEllipsis
 } from '@/components/ui/breadcrumb'
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
 export default function Files() {
   const { pathname } = useLocation()
   const path = decodeURIComponent(pathname.slice(1))
   const folders = path.split('/')
   return (
     <div className="px-2">
-      <Breadcrumb>
-        <BreadcrumbList>
-          {folders.map((folder, index, breadcrumbs) => {
-            const length = breadcrumbs.length
-            return (
-              <Fragment key={index}>
-                {index === length - 1 ? (
-                  <BreadcrumbPage>{folder}</BreadcrumbPage>
-                ) : (
-                  <BreadcrumbItem>{folder}</BreadcrumbItem>
-                )}
-                {index < length - 1 && <BreadcrumbSeparator />}
-              </Fragment>
-            )
-          })}
-        </BreadcrumbList>
-      </Breadcrumb>
+      {folders.length > 1 && (
+        <ScrollArea className="w-page">
+          <Breadcrumb>
+            <BreadcrumbList>
+              {folders.map((folder, index, breadcrumbs) => {
+                const length = breadcrumbs.length
+                const path = '/' + breadcrumbs.slice(0, index + 1).join('/')
+                return (
+                  <Fragment key={index}>
+                    {index === length - 1 ? (
+                      <BreadcrumbPage>{folder}</BreadcrumbPage>
+                    ) : (
+                      <BreadcrumbItem>
+                        <BreadcrumbLink asChild>
+                          <Link to={path}>{folder}</Link>
+                        </BreadcrumbLink>
+                      </BreadcrumbItem>
+                    )}
+                    {index < length - 1 && <BreadcrumbSeparator />}
+                  </Fragment>
+                )
+              })}
+            </BreadcrumbList>
+          </Breadcrumb>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+      )}
     </div>
   )
 }
